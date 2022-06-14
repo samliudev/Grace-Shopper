7824be81b0c59872212b6005e56822356f8566b5
ZillaR35 <lonnie.sheppard@rocketmail.com>
2022-06-14 16:58:44 -0400

    added validator

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -24,6 +24,9 @@ const User = db.define("user", {
     type: Sequelize.STRING,
     allowNull: false,
     unique: true,
+    validate: {
+      isEmail: true,
+    },
   },
   password: {
     type: Sequelize.STRING,
