14e548ef670e39bc769c501f36e4282e1c1e2dd2
TheWilsonMan <wilsontan1313@gmail.com>
2022-06-14 17:29:41 -0400

    Associations

--- server/db/index.js
+++ server/db/index.js
@@ -3,12 +3,30 @@
 const db = require('./db')
 
 const User = require('./models/User')
+const OrderDetails = require('./models/OrderDetails')
+const OrderItems = require('./models/OrderItems')
+const Product = require('./models/Product')
 
 //associations could go here!
+
+User.hasMany(OrderDetails)
+OrderDetails.belongsTo(User)
+
+OrderDetails.belongsToMany(Product, {through: "OrderDetailsProduct"} )
+Product.belongsToMany(OrderDetails, {through: "OrderDetailsProduct"} )
+
+OrderItems.hasOne(Product)
+Product.belongsTo(OrderItems)
+
+OrderDetails.hasMany(OrderItems)
+OrderItems.belongsTo(OrderDetails)
 
 module.exports = {
   db,
   models: {
     User,
+    OrderDetails,
+    OrderItems,
+    Product
   },
 }
