0e31128d46636cda15d18bfbbb616fbcd0345a30
TheWilsonMan <wilsontan1313@gmail.com>
2022-06-14 17:09:26 -0400

    OrderItems 2

--- server/db/models/OrderItems.js
+++ server/db/models/OrderItems.js
@@ -2,27 +2,19 @@ const Sequelize = require('sequelize');
 const db = require('./database');
 
 module.exports = db.define('OrderItems', {
-  OrderItemID:{
-    type: Sequelize.INTEGER,
-    allowNull: false,
-    unique: true
-  },
-  OrderDetailsID:{
-    type: Sequelize.INTEGER,
-    allowNull: false,
-    unique: true
-  },
-  PokemonID:{
-    type: Sequelize.INTEGER,
-    allowNull: false,
-    unique: true
-  },
   Quantity: {
     type: Sequelize.INTEGER,
     allowNull: false,
     validate: {
-      isInt: true
-    }
+      isInt: true,
+      min: 1
+    },
+  Price: {
+    type: Sequelize.DECIMAL(10,2),
+    validate: {
+      isDecimal: true,
+  }
+  }
   }
 
 })
