540b6f60356ba42bd0c26577aad8f6a7c181b6f5
TheWilsonMan <wilsontan1313@gmail.com>
2022-06-16 12:44:26 -0400

    Rerendering

--- client/components/AllPokemonView.js
+++ client/components/AllPokemonView.js
@@ -1,24 +1,34 @@
 import React, {useState, useEffect} from 'react'
-import pokemonArr from '../../seed';
+import axios from "axios"
 
 function AllPokemonView(){
-  const [pokemon, setPokemon] = useState(pokemonArr);
+  const [pokemon, setPokemon] = useState([]);
+  const [isFetching, setFetching] = useState(false)
+
+  useEffect(() => {
+    const fetchData = async() => {
+    const response = await fetch('http://localhost:8080/api/products')
+    const newData = await response.json()
+    setPokemon(newData)
+    }
+    fetchData()
+  },[pokemon])
+
+
+
+
 
   return (
     <div>
       {pokemon.map((pokemon,index) => {
         return(
           <div key = {pokemon.index}>
+          <img src = {pokemon.imageUrl}/>
           <p> Name: {pokemon.pokemon_name} </p>
           <p> Type: {pokemon.type} </p>
           <p> Price: {pokemon.price} </p>
           <p> Description: {pokemon.description} </p>
           <p> Quantity: {pokemon.quantity} </p>
-
-          <img src = {pokemon.imageUrl}/>
-
-          <p> Image: {pokemon.imageUrl} </p>
-
           </div>
         )
       })}
@@ -27,4 +37,4 @@ function AllPokemonView(){
 }
 
 
-export { AllPokemonView }
+export default AllPokemonView
