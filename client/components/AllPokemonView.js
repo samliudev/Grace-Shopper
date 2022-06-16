252282f49caad6abd88ecf9b2954ab4c7138c438
ZillaR35 <lonnie.sheppard@rocketmail.com>
2022-06-16 16:57:04 -0400

    navbar -axios

--- client/components/AllPokemonView.js
+++ client/components/AllPokemonView.js
@@ -1,40 +1,35 @@
-import React, {useState, useEffect} from 'react'
-import axios from "axios"
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function AllPokemonView(){
+function AllPokemonView() {
   const [pokemon, setPokemon] = useState([]);
-  const [isFetching, setFetching] = useState(false)
+  const [isFetching, setFetching] = useState(false);
 
   useEffect(() => {
-    const fetchData = async() => {
-    const response = await fetch('http://localhost:8080/api/products')
-    const newData = await response.json()
-    setPokemon(newData)
-    }
-    fetchData()
-  },[pokemon])
-
-
-
-
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:8080/api/products");
+      const newData = await response.json();
+      setPokemon(newData);
+    };
+    fetchData();
+  }, []);
 
   return (
     <div>
-      {pokemon.map((pokemon,index) => {
-        return(
-          <div key = {pokemon.index}>
-          <img src = {pokemon.imageUrl}/>
-          <p> Name: {pokemon.pokemon_name} </p>
-          <p> Type: {pokemon.type} </p>
-          <p> Price: {pokemon.price} </p>
-          <p> Description: {pokemon.description} </p>
-          <p> Quantity: {pokemon.quantity} </p>
+      {pokemon.map((pokemon, index) => {
+        return (
+          <div key={pokemon.index}>
+            <img src={pokemon.imageUrl} />
+            <p> Name: {pokemon.pokemon_name} </p>
+            <p> Type: {pokemon.type} </p>
+            <p> Price: {pokemon.price} </p>
+            <p> Description: {pokemon.description} </p>
+            <p> Quantity: {pokemon.quantity} </p>
           </div>
-        )
+        );
       })}
     </div>
-  )
+  );
 }
 
-
-export default AllPokemonView
+export default AllPokemonView;
