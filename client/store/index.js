a1d9d46e96da62304f542ba29b00d4d894a09887
TheWilsonMan <wilsontan1313@gmail.com>
2022-06-20 13:38:48 -0400

    NewReduxCart

--- client/store/index.js
+++ client/store/index.js
@@ -4,10 +4,13 @@ import thunkMiddleware from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import auth from './auth';
 import productsReducer from './products';
+import cartReducer from './cart';
 
-const reducer = combineReducers({ auth, productsReducer });
+const reducer = combineReducers({ auth, productsReducer, cartReducer });
 const middleware = composeWithDevTools(applyMiddleware(thunkMiddleware, createLogger({ collapsed: true })));
 const store = createStore(reducer, middleware);
+
+
 
 export default store;
 export * from './auth';
